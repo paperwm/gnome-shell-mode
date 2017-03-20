5df8f3bdcbef6ea81639d1ca9f97f018fd9f1b62
Tor Hedin Brønner <torhedinbronner@gmail.com>
2017-03-20 13:54:46 +0100

    Add helper script for completion

---
+++ local/gnome-shell-mode/emacs.js
@@ -0,0 +1,42 @@
+// -*- mode: gnome-shell; -*-
+
+emacs = {};
+
+emacs.verbose = true;
+
+const JsParse = imports.misc.jsParse;
+
+let commandHeader = 'const Clutter = imports.gi.Clutter; ' +
+                    'const GLib = imports.gi.GLib; ' +
+                    'const GObject = imports.gi.GObject; ' +
+                    'const Gio = imports.gi.Gio; ' +
+                    'const Gtk = imports.gi.Gtk; ' +
+                    'const Mainloop = imports.mainloop; ' +
+                    'const Meta = imports.gi.Meta; ' +
+                    'const Shell = imports.gi.Shell; ' +
+                    'const Main = imports.ui.main; ' +
+                    'const Lang = imports.lang; ' +
+                    'const Tweener = imports.ui.tweener; ' +
+                    /* Utility functions...we should probably be able to use these
+                     * in the shell core code too. */
+                    'const stage = global.stage; ' +
+                    /* Special lookingGlass functions */
+                    'const inspect = Lang.bind(Main.lookingGlass, Main.lookingGlass.inspect); ' +
+                    'const it = Main.lookingGlass.getIt(); ' +
+                    'const r = Lang.bind(Main.lookingGlass, Main.lookingGlass.getResult); ';
+
+let _getAutoCompleteGlobalKeywords = () => {
+    const keywords = ['true', 'false', 'null', 'new'];
+    // Don't add the private properties of window (i.e., ones starting with '_')
+    const windowProperties = Object.getOwnPropertyNames(window).filter(function(a){ return a.charAt(0) != '_' });
+    const headerProperties = JsParse.getDeclaredConstants(commandHeader);
+
+    return keywords.concat(windowProperties).concat(headerProperties);
+}
+
+emacs.completion_candidates = (text) => {
+    const AUTO_COMPLETE_GLOBAL_KEYWORDS = _getAutoCompleteGlobalKeywords();
+    let [completions, attrHead] = JsParse.getCompletions(text, commandHeader, AUTO_COMPLETE_GLOBAL_KEYWORDS);
+    return completions;
+};
+
