92500bc97a75c048feee64993e1a4e1725668936
Ole Jørgen Brønner <olejorgenb@yahoo.no>
2017-09-22 13:49:59 +0200

    Better pretty printing ground work
    Add custom pretty printers to `emacs.pretty_printers`
    
    Also catch errors in the pretty printing process to not confuse them with actual
    errors.
    
    A bit messy, but it'll do for now.

--- local/gnome-shell-mode/emacs.js
+++ local/gnome-shell-mode/emacs.js
@@ -11,7 +11,67 @@ emacs.list_properties = GObject.Object.list_properties;
 // Probably possible to extract from the error stack, but hardcode for now
 // Note: will change if newEval is redefined, restart gnome-shell when making
 // changes to this code for now
-emacs.eval_line_offset = 147;
+emacs.eval_line_offset = 207;
+
+/// Add custom printers here indexed by constructor name
+// (obj, key) => String or primitive to be serialized further to JSON
+// `key` is undefined if top-level object
+emacs.pretty_printers = {}
+
+emacs.pp_helper = function pp_helper(key, obj) {
+        let type = typeof(obj);
+        let pretty;
+        if (type === "undefined") {
+            pretty = "undefined";
+        } else if (obj === null) {
+            pretty = "null";
+        } else if (type === "object"
+                   && obj.constructor !== Object
+                   && obj.constructor !== Array) {
+            // Note: GObjects are not serialized properly by JSON.stringify
+            //       (and we want custom handling anyway)
+            let constructor_name = obj.constructor.name;
+            let custom_pp_fn = emacs.pretty_printers[constructor_name];
+            if (custom_pp_fn) {
+                pretty = custom_pp_fn(obj, key);
+            } else {
+                pretty = obj.toString();
+            }
+        } else if (type === "function") {
+            // Just print the whole definition
+            pretty = obj.toString();
+        } else if (type === "string") {
+            // Could special case string so we're sure it's easier to
+            // differentiate between a string and a custom string representation
+            // of an object. Eg. by surrounding it by single quotes.
+            pretty = obj
+        } else {
+            // Let JSON handle it
+            pretty = obj;
+        }
+        return pretty;
+    }
+
+emacs.pp_object = function(obj) {
+    if (obj !== null && typeof(obj) === "object"
+        && (obj.constructor === Object || obj.constructor === Array))
+    {
+        // Use JSON.stringify as a poor man's pretty printer for simple
+        // composite objects
+        return JSON.stringify(obj, emacs.pp_helper);
+    } else if(typeof(obj) === "string") {
+        // A pretty string have quotes around it to not conceal it's true nature
+        return JSON.stringify(obj);
+    } else {
+        // Top level simple or complex constructor
+        let pretty = emacs.pp_helper(undefined, obj);
+        if(typeof(pretty) !== "string") {
+            // Emacs expects a string, even for numbers
+            pretty = JSON.stringify(pretty);
+        }
+        return pretty;
+    }
+}
 
 emacs.originalDBusEval = imports.ui.shellDBus.GnomeShell.prototype.Eval;
 
@@ -25,27 +85,20 @@ imports.ui.shellDBus.GnomeShell.prototype.Eval = function newEval(code) {
             success: true,
         };
 
-        let type = typeof(eval_result);
-        if (type === "undefined") {
-            result.undefined = true;
-        } else if (eval_result === null) {
-            result.value = null;
-        } else if (type === "object" && eval_result.__metaclass__) {
-            // GObjects are not serialized properly by JSON.stringify
-            result.value = eval_result.toString();
-        } else if (type === "function") {
-            // Neither are functions
-            result.value = eval_result.toString();
-        } else {
-            // Use JSON.stringify as a poor man's pretty printer
-            result.value = JSON.stringify(eval_result);
+        try {
+            result.value = emacs.pp_object(eval_result)
 
-            // Also return the actual object. Currently used by the completion
-            // code to avoid parsing a pretty printed array.
-            // When/if we define our own dbus service we can have a separate
-            // dbus method for completion and maybe remove this line.
-            result.raw_value = eval_result;
+            if(eval_result && eval_result.constructor === Array) {
+                // Also return the actual object. Currently used by the
+                // completion code to avoid parsing a pretty printed array.
+                // When/if we define our own dbus service we can have a separate
+                // dbus method for completion and maybe remove this line.
+                result.raw_value = eval_result;
+            }
+        } catch(e) {
+            throw new Error("Error during pretty printing");
         }
+
     } catch(e) {
         // Note: JSON.stringify(e) doesn't reliably include all fields
         result = {
@@ -115,3 +168,29 @@ emacs.completion_candidates = (text) => {
     return completions.filter((x) => {return x.startsWith(attrHead); });
 };
 
+
+function pp_test_repl() {
+    "asdf"
+    a={a:gobj}
+    //: {"a":"[object instance proxy GType:MetaWindowX11 jsobj@0x7fa7beb07070 native@0x444eab0]"}
+    gobj = global.display.focus_window;
+    //: [object instance proxy GType:MetaWindowX11 jsobj@0x7fa7beb07070 native@0x444eab0]
+    emacs.pp_object(undefined)
+    //: "undefined"
+    emacs.pp_object(null)
+    //: "null"
+    emacs.pp_object(1)
+    "asdf"
+    a={a:gobj}
+    //: {"a":"[object instance proxy GType:MetaWindowX11 jsobj@0x7fa7beb07070 native@0x444eab0]"}
+    emacs.pp_object("a string")
+    //: "\"a string\""
+    a=emacs.pp_object({key:2})
+    //: "{\"key\":2}"
+    a=emacs.pp_object({key:"a string"})
+    //: "{\"key\":\"a string\"}"
+    a=emacs.pp_object({key:[gobj]})
+    //: "{\"key\":[\"[object instance proxy GType:MetaWindowX11 jsobj@0x7fa7beb07070 native@0x444eab0]\"]}"
+    JSON.stringify({key:2, nested:{a:2.3}})
+    //: "{\"key\":2,\"nested\":{\"a\":2.3}}"
+}
