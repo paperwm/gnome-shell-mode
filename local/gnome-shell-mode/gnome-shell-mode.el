paperwm/gnome-shell-mode

commit bf961826be2aa41296e0315b96e2910eceff3655

One off error in flycheck error position

local/gnome-shell-mode/gnome-shell-mode.el
@@ -108,7 +108,8 @@
                (buf-line   (+ (line-number-at-pos start) line))
                (buf-column (+ (save-excursion
                                 (goto-char start)
-                                (current-column)) column 1)))
+                                (current-column))
+                              column)))
 
           ;; FIXME: When using gnome-shell-repl the flycheck error is cleared for some reason?
           (flycheck-add-overlay 

commit 32291c31a48ef8afd9ac39598e87314976644264

Actually fix the one off linenumber errorr..
Fixes: bf961826be2aa41296e0315b96e2910eceff3655 (which adjusted the column instead)

Not sure what's going on with the column number from gjs. It seems to have
become less precise since gnome-shell 3.24

local/gnome-shell-mode/gnome-shell-mode.el
@@ -105,7 +105,7 @@
         ;; called
         (let* ((column (alist-get 'columnNumber result-obj))
                (line   (alist-get 'lineNumber result-obj))
-               (buf-line   (+ (line-number-at-pos start) line))
+               (buf-line   (+ (line-number-at-pos start) line -1)) ;; 1 vs 0 indexed
                (buf-column (+ (save-excursion
                                 (goto-char start)
                                 (current-column))