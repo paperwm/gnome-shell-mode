eff13e6458ef92acf302008409bade7951b9fc2b
Tor Hedin Brønner <torhedinbronner@gmail.com>
2017-12-08 22:08:55 +0100

    Complete more reserved words

--- local/gnome-shell-mode/gnome-shell-mode@hedning:matrix.org/emacs.js
+++ local/gnome-shell-mode/gnome-shell-mode@hedning:matrix.org/emacs.js
@@ -460,7 +460,7 @@ function findModule(moduleFilePath) {
 const JsParse = imports.misc.jsParse;
 
 let _getAutoCompleteGlobalKeywords = () => {
-    const keywords = ['true', 'false', 'null', 'new'];
+    const keywords = ['true', 'false', 'null', 'new', 'typeof', 'function', 'throw', 'catch', 'try'];
     // Don't add the private properties of window (i.e., ones starting with '_')
     const windowProperties = Object.getOwnPropertyNames(window).filter(function(a){ return a.charAt(0) != '_' });
 
