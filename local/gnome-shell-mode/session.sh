2c243a808bef8da95ebdc98a95c147c1e8656a7e
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-01-07 05:56:54 +0100

    session.sh: set monitors explicitly

--- local/gnome-shell-mode/session.sh
+++ local/gnome-shell-mode/session.sh
@@ -45,4 +45,7 @@ dconf write /org/gnome/shell/enabled-extensions "['${UUID}']"
 export CLUTTER_SHOW_FPS=1
 export SHELL_DEBUG=all
 export MUTTER_DEBUG=1
+export MUTTER_DEBUG_NUM_DUMMY_MONITORS=1
+export MUTTER_DEBUG_DUMMY_MONITOR_SCALES=1
+export MUTTER_DEBUG_TILED_DUMMY_MONITORS=1
 gnome-shell ${args[*]} 2>&1 | sed 's/\x1b\[[0-9;]*m//g'
